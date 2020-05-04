1596f1eaf47b5cf69d72981a9446575983d11537
TranXuanHoang <hoangtx.ict@gmail.com>
2020-05-05 00:22:51 +0900

    Add transition to category_meals_screen
    * Define new screen named category_meals_screen
    * Add route config for the gesture of tapping each category on the menu screen

--- food_menu/lib/category_item.dart
+++ food_menu/lib/category_item.dart
@@ -1,30 +1,46 @@
 import 'package:flutter/material.dart';
 
+import './category_meals_screen.dart';
+
 class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
 
   const CategoryItem({this.title, this.color});
 
+  void selectCategory(BuildContext context) {
+    // Navigate to the CategoryMealsScreen
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (_) {
+        return CategoryMealsScreen();
+      }),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(
-        title,
-        style: Theme.of(context).textTheme.title,
-      ),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            color.withOpacity(0.7),
-            color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.title,
         ),
-        borderRadius: BorderRadius.circular(20),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        padding: const EdgeInsets.all(20),
       ),
-      padding: const EdgeInsets.all(20),
     );
   }
 }
