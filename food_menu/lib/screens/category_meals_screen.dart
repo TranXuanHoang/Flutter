4ec04ceebcd3d3749d257fde8e83ca2a9ea9689c
TranXuanHoang <hoangtx.ict@gmail.com>
2020-05-05 15:33:39 +0900

    Reoganize the source code files

---
+++ food_menu/lib/screens/category_meals_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../dummy_data.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, Object>;
+    final String categoryId = routeArgs['id'];
+    final String categoryTitle = routeArgs['title'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: Center(
+        child: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return Text(categoryMeals[index].title);
+          },
+          itemCount: categoryMeals.length,
+        ),
+      ),
+    );
+  }
+}
