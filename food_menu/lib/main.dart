26af9b3b4528848af3985574c8c17e94a4dd0b8b
TranXuanHoang <hoangtx.ict@gmail.com>
2020-05-06 13:59:26 +0900

    Add a tab bar controlling the top page contents

--- food_menu/lib/main.dart
+++ food_menu/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import './screens/categories_screen.dart';
 import './screens/category_meals_screen.dart';
 import './screens/meal_detail_screen.dart';
+import './screens/tabs_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -34,7 +35,7 @@ class MyApp extends StatelessWidget {
       // home: CategoriesScreen(),
       initialRoute: '/', // default is '/'
       routes: {
-        '/': (ctx) => CategoriesScreen(),
+        '/': (ctx) => TabsScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
--- food_menu/lib/screens/categories_screen.dart
+++ food_menu/lib/screens/categories_screen.dart
@@ -6,28 +6,21 @@ import '../dummy_data.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Foods Menu",
-        ),
-      ),
-      body: GridView(
-        padding: const EdgeInsets.all(20),
-        children: DUMMY_CATEGORIES.map((catData) {
-          return CategoryItem(
-            id: catData.id,
-            title: catData.title,
-            color: catData.color,
-          );
-        }).toList(),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          // height:width ratio for child tiles (width = 200 => height = 300)
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
+    return GridView(
+      padding: const EdgeInsets.all(20),
+      children: DUMMY_CATEGORIES.map((catData) {
+        return CategoryItem(
+          id: catData.id,
+          title: catData.title,
+          color: catData.color,
+        );
+      }).toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        // height:width ratio for child tiles (width = 200 => height = 300)
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
     );
   }
