af34f12fd988936b40a379c4fb6485a1faf30b67
TranXuanHoang <hoangtx.ict@gmail.com>
2020-05-10 22:56:15 +0900

    Use Consumer instead of Provider.of to improve widget rebuild performance

--- online_shop/lib/widgets/product_item.dart
+++ online_shop/lib/widgets/product_item.dart
@@ -7,7 +7,10 @@ import '../screens/product_detail_screen.dart';
 class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    // set 'listen' to false to only get the product data first time without
+    // continuing to listen to the data change event. This is to avoid
+    // re-building the whole ClipRRect and its child widgets.
+    final product = Provider.of<Product>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GestureDetector(
@@ -24,14 +27,18 @@ class ProductItem extends StatelessWidget {
           ),
           footer: GridTileBar(
             backgroundColor: Colors.black87,
-            leading: IconButton(
-              icon: Icon(
-                product.isFavorite ? Icons.favorite : Icons.favorite_border,
+            // Use Consumer here to only rebuild the favorite IconButton
+            // when the state changes
+            leading: Consumer<Product>(
+              builder: (context, value, child) => IconButton(
+                icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border,
+                ),
+                color: Theme.of(context).accentColor,
+                onPressed: () {
+                  product.toggleIsFavorite();
+                },
               ),
-              color: Theme.of(context).accentColor,
-              onPressed: () {
-                product.toggleIsFavorite();
-              },
             ),
             title: Text(
               product.title,
