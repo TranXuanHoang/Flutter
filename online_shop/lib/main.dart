20277038deff1a500ef2c8d7effd70d6bfd37e72
TranXuanHoang <hoangtx.ict@gmail.com>
2020-05-11 08:45:17 +0900

    Define cart data model and register its data change notifier

--- online_shop/lib/main.dart
+++ online_shop/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import './providers/cart.dart';
 import './providers/products.dart';
 import './screens/product_detail_screen.dart';
 import './screens/products_overview_screen.dart';
@@ -13,8 +14,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => Products(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => Cart(),
+        ),
+      ],
       child: MaterialApp(
         title: 'Online Shop',
         theme: ThemeData(
---
+++ online_shop/lib/providers/cart.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final double price;
+  final int quantity;
+
+  CartItem({
+    this.id,
+    this.title,
+    this.price,
+    this.quantity,
+  });
+}
+
+class Cart with ChangeNotifier {
+  // Each element is a map of 'Product.id' and 'CartItem'
+  Map<String, CartItem> _items;
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addItem(String productId, double price, String title) {
+    if (_items.containsKey(productId)) {
+      // Already exist the same item, so just increase the quantity
+      _items.update(
+        productId,
+        (existingCartItem) => CartItem(
+          id: existingCartItem.id,
+          title: existingCartItem.title,
+          price: existingCartItem.price,
+          quantity: existingCartItem.quantity + 1,
+        ),
+      );
+    } else {
+      // Add a new item
+      _items.putIfAbsent(
+        productId,
+        () => CartItem(
+          id: DateTime.now().toString(),
+          title: title,
+          price: price,
+          quantity: 1,
+        ),
+      );
+    }
+  }
+}
