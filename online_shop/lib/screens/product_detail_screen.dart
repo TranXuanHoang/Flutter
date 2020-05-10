4ec0de4919192d7750ece085e4d3e254b8cbf435
TranXuanHoang <hoangtx.ict@gmail.com>
2020-05-10 15:35:32 +0900

    Use a centrally managed products list provider to fetch data for the product detail screen

--- online_shop/lib/screens/product_detail_screen.dart
+++ online_shop/lib/screens/product_detail_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = '/product-detail';
@@ -6,9 +9,14 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final String productId = ModalRoute.of(context).settings.arguments;
+    final loadedProduct = Provider.of<Products>(
+      context,
+      listen: false,
+    ).findById(productId);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(productId),
+        title: Text(loadedProduct.title),
       ),
     );
   }
