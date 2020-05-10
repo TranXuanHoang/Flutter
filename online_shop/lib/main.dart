7f62eb7bef581bb856a21538127857ad289323a4
TranXuanHoang <hoangtx.ict@gmail.com>
2020-05-10 13:28:17 +0900

    Add a products data provider and register data change listener
    * Define a data model for a list of products - this model inherits the ChangeNotifier so that its state change can be listened by other widgets
    * Add a ChangeNotifierProvider providing an instance of the products list to its decendants
    * Register a listener of products changes so that we can update the product-related widget(s) whenever the products list state changes

--- online_shop/lib/main.dart
+++ online_shop/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import './providers/products.dart';
 import './screens/product_detail_screen.dart';
 import './screens/products_overview_screen.dart';
 
@@ -11,21 +13,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Online Shop',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato',
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return ChangeNotifierProvider(
+      create: (context) => Products(),
+      child: MaterialApp(
+        title: 'Online Shop',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+          // This makes the visual density adapt to the platform that you run
+          // the app on. For desktop platforms, the controls will be smaller and
+          // closer together (more dense) than on mobile platforms.
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (context) => ProductDetailScreen(),
+        },
       ),
-      home: ProductsOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: (context) => ProductDetailScreen(),
-      },
     );
   }
 }
---
+++ online_shop/lib/widgets/products_grid.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './product_item.dart';
+import '../providers/products.dart';
+
+class ProductsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    final products = productsData.items;
+
+    return GridView.builder(
+      padding: EdgeInsets.all(10),
+      itemCount: products.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemBuilder: (ctx, i) {
+        return ProductItem(
+          id: products[i].id,
+          title: products[i].title,
+          imageUrl: products[i].imageUrl,
+        );
+      },
+    );
+  }
+}
