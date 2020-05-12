f2cf38d31712dbe7a6cd0162074d4559f5175e4d
TranXuanHoang <hoangtx.ict@gmail.com>
2020-05-13 02:35:18 +0900

    Add a side drawer for easier to navigate between screens
    * Define a common app_drawer widget
    * Add this drawer to the products overview (home) and orders history screen

--- online_shop/lib/screens/orders_screen.dart
+++ online_shop/lib/screens/orders_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/orders.dart' show Orders;
+import '../widgets/app_drawer.dart';
 import '../widgets/order_item.dart';
 
 class OrdersScreen extends StatelessWidget {
@@ -14,6 +15,7 @@ class OrdersScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Your Orders'),
       ),
+      drawer: AppDrawer(),
       body: ListView.builder(
         itemCount: ordersData.orders.length,
         itemBuilder: (context, i) => OrderItem(ordersData.orders[i]),
