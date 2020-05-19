c5f985b5d3a8f583bce2ce8ab2d2cd98e9fa91e2
TranXuanHoang <hoangtx.ict@gmail.com>
2020-05-19 19:51:51 +0900

    Update the favorite status optimistically

--- online_shop/lib/providers/product.dart
+++ online_shop/lib/providers/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,8 +20,33 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleIsFavorite() {
+  void _toggleIsFavoriteInLocalMemory() {
     isFavorite = !isFavorite;
     notifyListeners();
+  }
+
+  Future<void> toggleIsFavorite() async {
+    // Toggle the isFavorite in local memory first
+    _toggleIsFavoriteInLocalMemory();
+
+    // Update data from remote server using HTTP PATCH request
+    final url = 'https://flutter-update-67603.firebaseio.com/products/$id.json';
+    try {
+      final response = await http.patch(
+        url,
+        body: json.encode({
+          'isFavorite': isFavorite,
+        }),
+      );
+
+      // Roll back if the HTTP PATCH response is an error
+      if (response.statusCode >= 400) {
+        _toggleIsFavoriteInLocalMemory();
+      }
+    } catch (error) {
+      // Also roll back if other errors (i.e. network related errors) occur
+      print(error);
+      _toggleIsFavoriteInLocalMemory();
+    }
   }
 }
