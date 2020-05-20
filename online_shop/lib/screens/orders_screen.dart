6b9d60b6f65172c12689984bd5e031879d473561
TranXuanHoang <hoangtx.ict@gmail.com>
2020-05-20 21:32:17 +0900

    Improve the source code of fetching and showing orders
    * Use FutureBuilder widget to coordinate the asynchronous logic of loading orders data from the remote web server and showing up this data on the orders_screen

--- online_shop/lib/screens/orders_screen.dart
+++ online_shop/lib/screens/orders_screen.dart
@@ -5,70 +5,43 @@ import '../providers/orders.dart' show Orders;
 import '../widgets/app_drawer.dart';
 import '../widgets/order_item.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
-
-  @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isInit = true;
-  var _isLoading = false;
-
-  @override
-  void didChangeDependencies() async {
-    super.didChangeDependencies();
-    if (_isInit) {
-      _showLoading(true);
-      try {
-        await Provider.of<Orders>(context).fetchAndSetOrders();
-        _isInit = false;
-      } catch (error) {
-        showDialog(
-          context: context,
-          child: AlertDialog(
-            title: Text('Oops!'),
-            content: Text(
-                'Something went wrong. Please check your Internet connection and try again'),
-            actions: <Widget>[
-              FlatButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: Text('Got it'),
-              ),
-            ],
-          ),
-        );
-      } finally {
-        _showLoading(false);
-      }
-    }
-  }
-
-  void _showLoading(bool shouldShow) {
-    setState(() {
-      _isLoading = shouldShow;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    final ordersData = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Orders'),
       ),
       drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (context, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            // The data fetching is still under running
+            return Center(
               child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: ordersData.orders.length,
-              itemBuilder: (context, i) => OrderItem(ordersData.orders[i]),
-            ),
+            );
+          } else {
+            if (dataSnapshot.error != null) {
+              return Center(
+                child: Text(
+                  'Oops! Something went wrong. Please check your Internet connection and try again',
+                  textAlign: TextAlign.center,
+                ),
+              );
+            } else {
+              // Data fetching was done, no error occurred
+              return Consumer<Orders>(
+                builder: (context, ordersData, child) => ListView.builder(
+                  itemCount: ordersData.orders.length,
+                  itemBuilder: (context, i) => OrderItem(ordersData.orders[i]),
+                ),
+              );
+            }
+          }
+        },
+      ),
     );
   }
 }
