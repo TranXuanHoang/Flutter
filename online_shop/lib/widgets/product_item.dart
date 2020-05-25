59ab509baf576eca549f3bd150e8d12baa90369c
TranXuanHoang <hoangtx.ict@gmail.com>
2020-05-25 21:55:32 +0900

    Fade loaded images in on the products overview screen
    * Add a new assest image used as a placeholder image for products
    * Show the placeholder image while loading product images from the Internet
    * Use FadeInImage to show loaded images instead of showing the placeholder image shown at the beginning when product images are started to be loaded

--- online_shop/lib/widgets/product_item.dart
+++ online_shop/lib/widgets/product_item.dart
@@ -24,8 +24,11 @@ class ProductItem extends StatelessWidget {
           );
         },
         child: GridTile(
-          child: Image.network(
-            product.imageUrl,
+          child: FadeInImage(
+            placeholder: AssetImage('assets/images/product-placeholder.png'),
+            image: NetworkImage(
+              product.imageUrl,
+            ),
             fit: BoxFit.cover,
           ),
           footer: GridTileBar(
