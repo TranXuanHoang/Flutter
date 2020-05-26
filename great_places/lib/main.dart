8260038084a676c71065de918fcd9add3b0befb2
TranXuanHoang <hoangtx.ict@gmail.com>
2020-05-26 23:00:44 +0900

    Setup PlaceList and Places provider
    * Define Place data model
    * Define GreatPlaces as a ChangeNotifier that contains a list of Place items
    * Initialize places_list_screen with basic widget definition
    * Add 'provider' dependency to the project

--- great_places/lib/main.dart
+++ great_places/lib/main.dart
@@ -1,21 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './providers/great_places.dart';
+import './screens/places_list_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Great Places',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        accentColor: Colors.amber,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => GreatPlaces(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Great Places',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          accentColor: Colors.amber,
+          // This makes the visual density adapt to the platform that you run
+          // the app on. For desktop platforms, the controls will be smaller and
+          // closer together (more dense) than on mobile platforms.
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: PlacesListScreen(),
       ),
-      home: Text('Great Places'),
     );
   }
 }
