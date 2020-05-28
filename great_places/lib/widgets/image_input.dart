f60623fc9842da1face9011fd79ab657c0fb1737
TranXuanHoang <hoangtx.ict@gmail.com>
2020-05-28 09:59:19 +0900

    Take picture with camera and save image on Filesystem

--- great_places/lib/widgets/image_input.dart
+++ great_places/lib/widgets/image_input.dart
@@ -1,6 +1,11 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:image_picker_web/image_picker_web.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
 
 class ImageInput extends StatefulWidget {
   @override
@@ -8,7 +13,48 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
+  /// Saves file taken by Android/iOS devices
   File _storedImage;
+
+  /// Save image loaded by web browsers' image picker.
+  /// Determine whether to show on the image preview area by
+  /// looking at the kIsWeb.
+  Image _webBrowserImage;
+
+  Future<void> _takePicture() async {
+    // Android and iOS
+    if (!kIsWeb) {
+      // Take a photo using the camera of the device and
+      // show it in the image preview by using setState
+      final imageFile = await ImagePicker.pickImage(
+        source: ImageSource.camera,
+        maxWidth: 600,
+      );
+      setState(() {
+        _storedImage = imageFile;
+      });
+
+      // Store a copy of the photo taken into the app directory
+      // for later uses in the future
+      final appDir = await syspaths.getApplicationDocumentsDirectory();
+      final fileName = path.basename(imageFile.path);
+      final savedImage = await imageFile.copy('$appDir/$fileName');
+    }
+
+    // Web browsers
+    // @todo: Need to add logic to save the picked image chosen via
+    // the image picker of the web browsers
+    if (kIsWeb) {
+      Image fromPicker =
+          await ImagePickerWeb.getImage(outputType: ImageType.widget);
+
+      if (fromPicker != null) {
+        setState(() {
+          _webBrowserImage = fromPicker;
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,16 +69,18 @@ class _ImageInputState extends State<ImageInput> {
               color: Colors.grey,
             ),
           ),
-          child: _storedImage != null
-              ? Image.file(
-                  _storedImage,
-                  fit: BoxFit.cover,
-                  width: double.infinity,
-                )
-              : Text(
-                  'No Image Taken',
-                  textAlign: TextAlign.center,
-                ),
+          child: kIsWeb
+              ? _webBrowserImage
+              : _storedImage != null
+                  ? Image.file(
+                      _storedImage,
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                    )
+                  : Text(
+                      'No Image Taken',
+                      textAlign: TextAlign.center,
+                    ),
           alignment: Alignment.center,
         ),
         SizedBox(width: 10),
@@ -41,7 +89,7 @@ class _ImageInputState extends State<ImageInput> {
             icon: Icon(Icons.camera),
             label: Text('Take Picture'),
             textColor: Theme.of(context).primaryColor,
-            onPressed: () {},
+            onPressed: _takePicture,
           ),
         ),
       ],
