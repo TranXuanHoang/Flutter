c04faff8dad2533dd2b94cc53c868f636f0f85cd
TranXuanHoang <hoangtx.ict@gmail.com>
2020-06-02 02:42:01 +0900

    Setup Firebase SDK for Android app
    * Add Flutter cloud_firestore dependency  to use the [Cloud Firestore API](https://firebase.google.com/docs/firestore/)
    * Config Firebase SDK for the Android app. See [Firebase doc](https://firebase.google.com/docs/flutter/setup?platform=android) for more information about config steps
    * Try Firestore's methods to retrieve data from a Firebase's [Cloud Firestore](https://firebase.google.com/docs/firestore)

--- instant_chat/lib/screens/chat_screen.dart
+++ instant_chat/lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -10,6 +11,19 @@ class ChatScreen extends StatelessWidget {
           padding: EdgeInsets.all(8),
           child: Text('This works!'),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance
+              .collection('chats/hZ25P0awp3qvsGfTbAdP/messages')
+              .snapshots()
+              .listen((data) {
+            data.documents.forEach((message) {
+              print(message['text']);
+            });
+          });
+        },
       ),
     );
   }
