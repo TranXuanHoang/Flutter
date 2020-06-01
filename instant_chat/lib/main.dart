3378b520565021ddff37c3dc2094c54cdbddfb9b
TranXuanHoang <hoangtx.ict@gmail.com>
2020-06-01 09:27:51 +0900

    Initialize instant chat app

---
+++ instant_chat/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import './screens/chat_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
