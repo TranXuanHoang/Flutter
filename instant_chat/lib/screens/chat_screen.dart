b4febecf7a17b7db870d59c12e647a5b64dd5041
TranXuanHoang <hoangtx.ict@gmail.com>
2020-06-07 22:39:29 +0900

    Configure Firebase Cloud Messaging for push notifications
    * Add firebase_messaging dependency to work with Firebase [Cloud Messaging](https://firebase.google.com/docs/cloud-messaging)
    * Add setup of the [firebase_messaging](https://pub.dev/packages/firebase_messaging) dependency for Android. Need to add the setup for iOS if want to add push notifications to iOS devices

--- instant_chat/lib/screens/chat_screen.dart
+++ instant_chat/lib/screens/chat_screen.dart
@@ -1,10 +1,42 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/chat/messages.dart';
 import '../widgets/chat/new_message.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Request permissions for receiving Push Notifications.
+    // This will bring up a permissions dialog for the user to confirm on iOS.
+    // It's a no-op on Android. For more detail about this, head over to
+    // https://pub.dev/packages/firebase_messaging#dartflutter-integration
+    final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+    _firebaseMessaging.requestNotificationPermissions();
+    _firebaseMessaging.configure(
+      onMessage: (message) {
+        print('onMessage: $message');
+        return;
+      },
+      onResume: (message) {
+        print('onResume: $message');
+        return;
+      },
+      onLaunch: (message) {
+        print('onLaunch: $message');
+        return;
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
