06168fea477293059b39cbd6f200da944d60c627
TranXuanHoang <hoangtx.ict@gmail.com>
2020-06-09 11:30:41 +0900

    Create a Flutter platform client to call specific-platform code
    See [create the Flutter platform client](https://flutter.dev/docs/development/platform-integration/platform-channels#example-client) documentation for more information

--- battery_level/lib/main.dart
+++ battery_level/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,6 +27,27 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  static const platform = MethodChannel('example.hoang.com/battery');
+  int _batteryLevel;
+
+  /// Connects with platform-specific iOS and Android code using platform channels.
+  /// See Flutter
+  /// [Writing custom platform-specific code](https://flutter.dev/docs/development/platform-integration/platform-channels)
+  /// for more information on how to connect with native code in Flutter.
+  Future<void> _getBatteryLevel() async {
+    try {
+      final batteryLevel = await platform.invokeMethod('getBatteryLevel');
+      setState(() {
+        _batteryLevel = batteryLevel;
+      });
+    } on PlatformException catch (error) {
+      print(error);
+      setState(() {
+        _batteryLevel = null;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,7 +55,21 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text('Native Code'),
       ),
       body: Center(
-        child: Text('Battery Level...'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text(
+              'Battery Level: $_batteryLevel',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            RaisedButton(
+              onPressed: _getBatteryLevel,
+              child: Text('Get Battery Level'),
+            ),
+          ],
+        ),
       ),
     );
   }
